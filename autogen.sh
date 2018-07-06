42a15e08a8bd6cdcb701cb8b7895fad11cbe484c
Jeffrey Stedfast <jestedfa@microsoft.com>
2018-07-06 14:51:04 -0400

    Regenerated CoreML project based on Apple's new version of CoreML (Xcode10)

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,4 @@
 SRC_DIR=external/coremltools/mlmodel/format
 DST_DIR=CoreML
 
-protoc -I=$SRC_DIR --csharp_out=$DST_DIR $SRC_DIR/Model.proto
+protoc -I=$SRC_DIR --csharp_out=$DST_DIR $SRC_DIR/*.proto
